7ca0fbcc20df41688e2941d463102b686d4c9803
aditi1403 <72371906+aditi1403@users.noreply.github.com>
2020-10-05 10:34:10 +0530

    Create apni_dictionary.py

---
+++ code with harry projects/apni_dictionary.py
@@ -0,0 +1,7 @@
+# python-tutorials
+n=input()
+d1={"set":"a well defined collection of objects",
+    "mutable":"can change",
+     "immutable":"cannot change",
+    "sort":"to arrange in a particular manner"}
+print(d1[n])

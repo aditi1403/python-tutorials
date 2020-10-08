7fb2918087c5f596fc7e669f6b2bb7d6c0fdd152
aditi1403 <72371906+aditi1403@users.noreply.github.com>
2020-10-08 18:47:20 +0530

    Create fibonacci_series_term.py

---
+++ code with harry projects/fibonacci_series_term.py
@@ -0,0 +1,12 @@
+# FIBONACCI SERIES
+
+n=int(input("enter the nth term\n"))
+def fibo(n):
+    if n==1:
+        return 0
+    elif n==2:
+        return 1
+    else:
+        return fibo(n-1)+fibo(n-2)
+print(fibo(n))
+print("your ",n,"th term in the fibonacci series is:")
